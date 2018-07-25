94ee79dd0ccffdc3f2c7f90bcab8a029d37532b8
marchuanv <marchuanv@gmail.com>
2018-07-25 22:05:23 +0200

    fix

--- app.js
+++ app.js
@@ -2,14 +2,21 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
+const appDir=path.join(__dirname,'app');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname,'../app')));
-app.use(express.static(path.join(__dirname,'app')));
+app.use(express.static(appDir));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,'app','index.html'));
+    res.sendFile(path.join(appDir,'index.html'));
 });
+
+fs.readdir(appDir, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+})
+
 
 app.listen(port, () => console.log(`listening on ${port}`));

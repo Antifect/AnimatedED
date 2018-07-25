9057ce844f6d6b4f95cb58a0c1ca3f181d9ba45a
marchuanv <marchuanv@gmail.com>
2018-07-25 21:46:11 +0200

    setup

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -9,5 +10,11 @@ app.use(express.static('app'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,'app','index.html'));
 });
+
+fs.readdir(__dirname, (err, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
+})
 
 app.listen(port, () => console.log(`listening on ${port}`));

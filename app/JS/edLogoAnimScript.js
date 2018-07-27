d5c65cfb42c2edb501f21ac215edd5dda277b470
Edward <31430546+Antifect@users.noreply.github.com>
2018-07-27 11:28:59 +0200

    Update edLogoAnimScript.js
    added a click event to the background to play the animation

--- app/JS/edLogoAnimScript.js
+++ app/JS/edLogoAnimScript.js
@@ -1,7 +1,18 @@
-var animation = bodymovin.loadAnimation ({
-     container:document.getElementById('edLogoAnimBm'),
+var animation;
+
+var animationData = {
+    container:document.getElementById('edLogoAnimBm'),
     renderer:'svg',
-    loop:true,
+    loop:false,
     autoplay:true,
     path: 'JS/edLogoAnim.json'
-})
+};
+
+animation = bodymovin.loadAnimation(animationData);
+
+var x = document.getElementById('home');
+x.addEventListener('click', animate);
+
+function animate() {
+    animation.goToAndPlay(1, true);
+}

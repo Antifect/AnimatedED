a564c2f278a0dd12668a0dc6bdee4ac471746273
Edward <31430546+Antifect@users.noreply.github.com>
2018-08-01 18:12:17 +0200

    Add files via upload

---
+++ app/JS/mobileNavScript.js
@@ -0,0 +1,8 @@
+function mobileNavFunction() {
+    var x = document.getElementById("topNav");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}

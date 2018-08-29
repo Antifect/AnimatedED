d77a464465a12696457bb0a385d3557462d1413d
Edward <31430546+Antifect@users.noreply.github.com>
2018-08-29 15:19:04 +0200

    Update skillsIconAnimScript.js
    added a function to animate the animation data

--- app/JS/skillsIconAnimScript.js
+++ app/JS/skillsIconAnimScript.js
@@ -1,7 +1,13 @@
-var animation = bodymovin.loadAnimation ({
-     container:document.getElementById('skillsBm'),
+var animationData = {
+    container:document.getElementById('skillsBm'),
     renderer:'svg',
     loop:true,
     autoplay:true,
     path: 'JS/skillsIconAnim.json'
-})
+};
+
+var animation = bodymovin.loadAnimation(animationData);
+
+function animate () {
+    animation.paly();
+};
